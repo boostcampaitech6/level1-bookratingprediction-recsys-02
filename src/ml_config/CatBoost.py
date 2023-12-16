e00d4337e4f56fbcd0f2704bfc1583e76c22e782
twndus <twndus1362@gmail.com>
2023-12-16 11:07:00 +0000

    feat: ml config module #29

---
+++ src/ml_config/CatBoost.py
@@ -0,0 +1,8 @@
+CatBoostConfig = {
+    'learning_rate': 1e-3,
+    'depth': 4,
+    'iterations': 1000,
+    'min_child_samples': 40,
+    'grow_policy': 'Depthwise',
+    'early_stopping_rounds': 100,
+}
